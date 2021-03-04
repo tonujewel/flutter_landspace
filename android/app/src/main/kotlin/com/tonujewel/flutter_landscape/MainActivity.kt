756bca597ace6e8c0f907a743a4493a8224cb512
jewelrana <tonujewel@gmail.com>
2021-03-04 22:42:24 +0600

    init

---
+++ android/app/src/main/kotlin/com/tonujewel/flutter_landscape/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tonujewel.flutter_landscape
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget portrait() {
+    return Center(
+      child: Text("portrait"),
+    );
+  }
+
+  Widget landscape() {
+    return Center(
+      child: Text("landscape"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return portrait();
+          } else {
+            return landscape();
+          }
+        },
+      ),
+    );
+  }
+}
